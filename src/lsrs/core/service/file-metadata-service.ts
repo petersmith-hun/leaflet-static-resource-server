72413ff5b0d5aab3ad39e958504f4e0901f86dee
Péter Kovács <peter.kovacs0519@gmail.com>
2022-12-05 21:26:26 +0100

    LFLT-522 Move file metadata handling related operations to LSRS  - Implemented FileMetadataService  - Added unit tests  - Bumped application version to 0.3.0-dev

---
+++ src/lsrs/core/service/file-metadata-service.ts
@@ -0,0 +1,95 @@
+import {UniqueConstraintError} from "sequelize";
+import {Inject, Service} from "typedi";
+import UploadedFileDAO from "../dao/uploaded-file-dao";
+import {ConflictingResourceError, GenericError, ResourceNotFoundError} from "../error/error-types";
+import {
+    UploadedFile,
+    UploadedFileCreateAttributes,
+    UploadedFileDescriptor,
+    UploadedFileUpdateAttributes
+} from "../model/uploaded-file";
+
+/**
+ * Uploaded file metadata operations interface.
+ */
+@Service()
+export default class FileMetadataService {
+
+    private readonly uploadedFileDAO: UploadedFileDAO;
+
+    constructor(@Inject() uploadedFileDAO: UploadedFileDAO) {
+        this.uploadedFileDAO = uploadedFileDAO;
+    }
+
+    /**
+     * Retrieves metadata for given pathUUID.
+     *
+     * @param pathUUID pathUUID of the file to retrieve metadata of
+     * @returns metadata as UploadedFileDescriptor, wrapped in Promise
+     */
+    async retrieveMetadata(pathUUID: string): Promise<UploadedFileDescriptor> {
+
+        const uploadedFile = await this.uploadedFileDAO.findByPathUUID(pathUUID);
+        if (!uploadedFile) {
+            throw new ResourceNotFoundError(UploadedFile, pathUUID);
+        }
+
+        return Promise.resolve(uploadedFile.dataValues);
+    }
+
+    /**
+     * Stores metadata of uploaded file.
+     *
+     * @param uploadedFile uploaded file information as UploadedFileCreateAttributes
+     * @returns ID of created metadata record
+     */
+    async storeMetadata(uploadedFile: UploadedFileCreateAttributes): Promise<number> {
+
+        let storedUploadedFile: UploadedFileDescriptor;
+        try {
+            storedUploadedFile = await this.uploadedFileDAO.save(uploadedFile)
+        } catch (error) {
+            if (error instanceof UniqueConstraintError) {
+                throw new ConflictingResourceError(UploadedFile, uploadedFile.pathUUID);
+            } else {
+                throw new GenericError(`Failed to store metadata record: ${error}`)
+            }
+        }
+
+        return Promise.resolve(storedUploadedFile.id);
+    }
+
+    /**
+     * Removes metadata for given file by pathUUID.
+     *
+     * @param pathUUID pathUUID of the file to remove metadata of
+     */
+    async removeMetadata(pathUUID: string): Promise<void> {
+
+        const uploadedFile = await this.retrieveMetadata(pathUUID);
+        await this.uploadedFileDAO.delete(uploadedFile.id);
+    }
+
+    /**
+     * Updates file metadata.
+     *
+     * @param pathUUID pathUUID of existing file to update metadata of
+     * @param updatedMetadata updated metadata as UploadedFileUpdateAttributes
+     */
+    async updateMetadata(pathUUID: string, updatedMetadata: UploadedFileUpdateAttributes): Promise<void> {
+
+        const uploadedFile = await this.retrieveMetadata(pathUUID);
+        await this.uploadedFileDAO.update(uploadedFile.id, updatedMetadata);
+    }
+
+    /**
+     * Retrieves metadata for all uploaded files.
+     *
+     * @returns list of uploaded files as UploadedFileDescriptor array, wrapped in Promise
+     */
+    async getUploadedFiles(): Promise<UploadedFileDescriptor[]> {
+
+        return (await this.uploadedFileDAO.findAll())
+            .map(uploadedFile => uploadedFile.dataValues);
+    }
+}
